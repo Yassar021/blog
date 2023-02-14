8777902a04569c8e5e25d6700f4adbb491cdf8cb
Yassar021 <yassar.tkj114@gmail.com>
2023-02-14 13:24:21 +0700

    update

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Simpeka Web Dashboard',
+    description: `simpeka is a web app that is engaged in collecting data used in the TOEFL program at Haluleo universities using Laravel as the main language.`,
+    imgSrc: '/static/images/simpeka.png',
+    href: 'https://simpeka--uptbahasauho.id/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Kedai Booking Web App',
+    description: `Kedai Kilometer App is Booking App for Coffee Shop we are using Next JS and Laravel`,
+    imgSrc: '/static/images/kedai-kilometer.png',
+    href: '/',
   },
 ]
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Helloooo',
+  author: 'Ahmad Muyassar',
+  headerTitle: 'Ahmad Muyassar',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -10,12 +10,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'yassar.tkj114@gmail.com',
+  github: 'https://github.com/Yassar021',
+  // twitter: 'https://twitter.com/Twitter',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/ahmad-muyassar-yani-76183b114/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
